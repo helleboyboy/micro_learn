ab92133073b02b1dc073342627ef0c8376173174
java页 <1786076351@qq.com>
2022-12-29 01:37:43 +0800

    info_8014模块：用接口来获取到register信息，避免重复开发

--- info_8014/src/main/java/com/javaye/info/service/impl/UnionServiceImpl.java
+++ info_8014/src/main/java/com/javaye/info/service/impl/UnionServiceImpl.java
@@ -6,9 +6,12 @@ import com.javaye.info.mapper.RegisterMapper;
 import com.javaye.info.mapper.UnionInfoRegisterMapper;
 import com.javaye.info.service.UnionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @Author: Java页大数据
@@ -22,20 +25,37 @@ public class UnionServiceImpl implements UnionService {
     @Autowired
     RegisterMapper registerMapper;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @Override
     public List<UnionInfoAndRegister> selectAll() {
         List<UnionInfoAndRegister> unionInfoAndRegisters = unionInfoRegisterMapper.selectList(null);
+
 //        可是这个就相当于 重复开发了！！！需要改善！
-        List<Register> registers = registerMapper.selectList(null);
+//        List<Register> registers = registerMapper.selectList(null);
+//        for (UnionInfoAndRegister unionInfoAndRegister : unionInfoAndRegisters) {
+//            String name = unionInfoAndRegister.getName();
+//            for (Register register : registers) {
+////                根据name字段相等来进行组合
+//                if (register.getName().equals(name)) {
+//                    unionInfoAndRegister.setRegister(register);
+//                }
+//            }
+//        }
+
+        String url = "http://localhost:8011/query/registerAll";
+        Register[] forObject = restTemplate.getForObject(url, Register[].class);
         for (UnionInfoAndRegister unionInfoAndRegister : unionInfoAndRegisters) {
             String name = unionInfoAndRegister.getName();
-            for (Register register : registers) {
-//                根据name字段相等来进行组合
-                if (register.getName().equals(name)) {
+            for (int i = 0; i < Objects.requireNonNull(forObject).length; i++) {
+                Register register = (Register) forObject[i];
+                if (register.getName().equals(name)){
                     unionInfoAndRegister.setRegister(register);
                 }
             }
         }
+
         return unionInfoAndRegisters;
     }
 }
